88c9b176a7a55b8a4b7e26126aeaaf78962cf064
TWkim <mll523@naver.com>
2026-05-14 00:26:31 +0900

    로그인 문제 해결(테스트)

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,10 @@ const nextConfig: NextConfig = {
         source: '/__/auth/:path*',
         destination: 'https://web-novel-viewer.firebaseapp.com/__/auth/:path*',
       },
+      {
+        source: '/__/firebase/:path*',
+        destination: 'https://web-novel-viewer.firebaseapp.com/__/firebase/:path*',
+      },
     ];
   },
 };
